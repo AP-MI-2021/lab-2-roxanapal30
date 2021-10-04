67e4dbcd87a1f63a5e663b61e36e0aba1cadf4d3
Roxana Paladi <roxana300302@gmail.com>
2021-10-04 19:25:09 +0300

    am rezolvat problema 3

--- lab2.py
+++ lab2.py
@@ -45,24 +45,51 @@ def is_antipalindrome(n):
         return True
     else:
         return False
+'''
+functia get_perfect_squares(n, m) cauta si afiseaza toate patratele perfecte din intervalul [n,m]
+'''
+
+def get_perfect_squares(n, m):
+    patrateperf=[]
+    shouldRun=True
+    i=1
+    while shouldRun:
+       i=i+1
+       if i*i>m:
+            shouldRun=False
+       if i*i>=n:
+           if i*i<=m:
+            patrateperf.append(i*i)
+
+    return patrateperf
 
 def test_get_largest_prime_below():
     assert get_largest_prime_below(9) == 7
     assert get_largest_prime_below(1) == 0
+    assert get_largest_prime_below(15) == 13
 
 def test_is_antipalindrome():
     assert is_antipalindrome(121) is True
     assert is_antipalindrome(123) is False
+    assert is_antipalindrome(1245) is False
+
+def test_get_perfect_squares():
+    assert get_perfect_squares(2, 9) == [4,9]
+    assert get_perfect_squares(1, 5) == [4]
+    assert get_perfect_squares(13, 40) == [16,25,36]
 
 def main():
     test_get_largest_prime_below()
     test_is_antipalindrome()
+    test_get_perfect_squares()
     n=int(input("Dati nr.:"))
+    m=int(input("Dati nr pentru patrate perfecte:"))
     get_largest_prime_below(n)
     print(get_largest_prime_below(n))
     if is_antipalindrome(n) ==True:
         print("este palindrom")
     else:
         print("nu este palindrom")
+    print(get_perfect_squares(n,m))
 
 main()
